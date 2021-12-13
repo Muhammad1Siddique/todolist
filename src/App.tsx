599141fd684617bbad8774123422f9948f7ff555
Muhammad Siddique <48084054+Muhammad1Siddique@users.noreply.github.com>
2021-12-13 14:35:53 +0500

    Todolist Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { TodoList } from './TodoList';
+import { AddTodoForm } from './AddTodoForm';
+
+const initialTodos: Todo[] = [
+  {
+    text: 'Walk the dog',
+    complete: false,
+  },
+  {
+    text: 'Write app',
+    complete: true,
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+  const toggleTodo = (selectedTodo: Todo) =>{
+    const newTodos = todos.map((todo)=>{
+      if(todo===selectedTodo){
+        return{
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const addTodo: AddTodo = (text:string) =>{
+    const newTodo = { text, complete: false };
+    setTodos([...todos, newTodo]);
+  }
+
   return (
     <>
-      <h2>Hello World!</h2>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <AddTodoForm addTodo={addTodo} />
     </>
   );
 }
